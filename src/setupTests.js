8969f5a53e45532fb61ec8c745dd19fc50c0a607
Matias Hamie <47436616+MatiasHamie@users.noreply.github.com>
2021-05-21 16:53:24 -0300

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,12 +1,13 @@
+// Instalar todo esto
+// npm install --save-dev @wojtekmaj/enzyme-adapter-react-17 enzyme-to-json enzyme @testing-library/react-hooks
+
 // import "@testing-library/jest-dom/extend-expect";
 
 // sacado de 
 // Enzyme (ojo, solo para version <16 de React) https://github.com/wojtekmaj/enzyme-adapter-react-17
 // Enzyme para React version >17 https://github.com/wojtekmaj/enzyme-adapter-react-17
 // Para los snapshots de wrappers instalar https://www.npmjs.com/package/enzyme-to-json
-// npm install --save-dev enzyme-to-json
 
-// npm install --save-dev @wojtekmaj/enzyme-adapter-react-17 enzyme (no olvidar ese enzyme ultimo)
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import {createSerializer} from 'enzyme-to-json';
